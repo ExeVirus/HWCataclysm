9fcc50298445c90086dc826797ec63aa97534e5a
Taiidans <43096388+Taiidans@users.noreply.github.com>
2019-11-20 17:39:44 +0000

    Add files via upload

---
+++ Campaign/Mission01/teamcolour.lua
@@ -0,0 +1,14 @@
+-- 0 = player
+-- 1 = Kiith Nabaal
+-- 2 = Hiigaran Denfse Fleet
+-- 3 = Taiidan Imperailsits 
+
+teamcolours = {
+	[0] = {{1, 1, 1},             {0, 0.502, 0.502},     "DATA:Badges/beast slayer.tga", {1, 1, 1},             "data:/effect/trails/tai_trail_clr.tga"}, 
+	[1] = {{0.365, 0.553, 0.667}, {0.8, 0.8, 0.8},       "DATA:Badges/cnabaal.tga",      {0.365, 0.553, 0.667}, "data:/effect/trails/hgn_trail_clr.tga"},
+	[2] = {{1, 1, 1},             {0, 0.502, 0.502},     "DATA:Badges/bentusi.tga", {1, 1, 1},             "data:/effect/trails/tai_trail_clr.tga"},  
+	[3] = {{1,0.823529411764706,0},             {1, 0, 0},     "DATA:Badges/imperial taiidan.tga", {0.921,0.75,0.419},             "data:/effect/trails/tai_trail_clr.tga"}, 
+}
+
+
+
